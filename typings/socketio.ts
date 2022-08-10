1ff4d7b46d8a9d793a0e2ee61d2d9d15f6b68d07
zznixt07 <zznixt07@protonmail.com>
2022-08-10 22:45:46 +0545

    unignore typings/

---
+++ typings/socketio.ts
@@ -0,0 +1,20 @@
+export interface ServerToClientEvents {
+	noArg: () => void
+	basicEmit: (a: number, b: string, c: Buffer) => void
+	withAck: (d: string, callback: (e: number) => void) => void
+	
+}
+
+export interface ClientToServerEvents {
+
+}
+
+export interface InterServerEvents {
+	ping: () => void
+}
+
+export interface SocketData {
+	name: string
+	age: number
+
+}
